5eb96a5a353128144a0c545d8d1c0f10a1ed9e8c
Anton Tabar <atabar2@users.noreply.github.com>
2021-01-28 20:39:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ lcd = lcddriver.lcd()
 lcd.lcd_clear()
  
 lcd.lcd_display_string("My words -", 1)
-lcd.lcd_display_string("    In VC", 2)
+lcd.lcd_display_string("Coming from web", 2)
 # lcd.lcd_display_string("", 3)
 # lcd.lcd_display_string("HD44780 I2C Tutorial", 4)
