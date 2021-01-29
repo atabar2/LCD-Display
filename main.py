31aa6e88d8d64c6d9e135f83a989880177991450
Anton Tabar <atabar2@gmail.com>
2021-01-28 20:34:15 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is the main driver stub file
+
+# First edit from the web side to test sync
+
+# First edit from RaspPi 3
+
+import lcddriver
+from time import *
+ 
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
+ 
+lcd.lcd_display_string("My words -", 1)
+lcd.lcd_display_string("    In VC", 2)
+# lcd.lcd_display_string("", 3)
+# lcd.lcd_display_string("HD44780 I2C Tutorial", 4)
