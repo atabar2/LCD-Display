bed4da06db9f1f04c0f6eb9e65f06671f4768206
Anton Tabar <atabar2@gmail.com>
2021-01-29 11:13:30 -0800

    Shortened stub routines

--- main.py
+++ main.py
@@ -9,23 +9,10 @@ from datetime import datetime
 lcd = lcddriver.lcd()
 lcd.lcd_clear()
  
-lcd.lcd_display_string("My words -", 1)
-lcd.lcd_display_string("Coming from web", 2)
-lcd.lcd_display_string("Going nowhere", 3)
+lcd.lcd_display_string("Demo of LCD Display", 1)
+lcd.lcd_display_string(" ", 2)
+lcd.lcd_display_string("Display size: 20", 3)
 lcd.lcd_display_string("12345678901234567890", 4)
-
-sleep(3)
-
-lcd.lcd_display_string("Turning off light", 4)
-sleep(2)
-
-lcd.lcd_backlight("Off")
-
-sleep(3)
-
-lcd.lcd_display_string("And Magically On", 4)
-sleep(.1)
-lcd.lcd_backlight("On")
 
 sleep(5)
 
